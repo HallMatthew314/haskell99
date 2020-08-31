c06b6679347de3625da61774473727094d75f821
Matthew Hall <hallmg1@student.op.ac.nz>
2020-08-31 13:00:10 +1200

    11 and 12

--- h99.hs
+++ h99.hs
@@ -84,3 +84,27 @@ packHelp (x:y:xs) = if head x == head y
 encode :: (Eq a) => [a] -> [(Int, a)]
 encode xs = map (\t -> (length t, head t)) (pack xs)
 
+-- 11. (*) Modified run-length encoding.
+-- Modify the result of problem 10 in such a way that if an element
+-- has no duplicates it is simply copied into the result list.
+-- Only elements with duplicates are transferred as (N E) lists.
+data MultiSingle a = Single a
+                   | Multiple Int a
+                   deriving (Show)
+
+msConv :: (Int, a) -> MultiSingle a
+msConv t = if fst t == 1
+           then Single $ snd t
+           else Multiple (fst t) (snd t)
+
+encodeModified :: (Eq a) => [a] -> [MultiSingle a]
+encodeModified xs = map msConv (encode xs)
+
+-- 12. (**) Decode a run-length encoded list.
+-- Given a run-length code list generated as specified in problem 11,
+-- construct its uncompressed version.
+decodeModified :: [MultiSingle a] -> [a]
+decodeModified []                  = []
+decodeModified ((Single x):xs)     = x : decodeModified xs
+decodeModified ((Multiple c x):xs) = (take c $ repeat x) ++ decodeModified xs
+
