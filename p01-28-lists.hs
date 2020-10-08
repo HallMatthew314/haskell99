0fd05042777607c51e07e1edb7884279e8449b65
Matthew Hall <hallmg1@student.op.ac.nz>
2020-10-08 22:12:02 +1300

    Combinatorics suuuucks

--- p01-28-lists.hs
+++ p01-28-lists.hs
@@ -231,3 +231,11 @@ diffSelect n m = rndSelect [1..m] n
 rndPermu :: [a] -> IO [a]
 rndPermu = shuffle
 
+-- 26. (**) Generate the combinations of K distinct objects
+-- chosen from the N elements of a list.
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = [[]]
+combinations _ [] = []
+combinations n (x:xs) =
+  map (x:) (combinations (n - 1) xs) ++ combinations n xs
+
